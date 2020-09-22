6caeca3195a34067759237a56c2887f4e66fc534
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-09-22 16:29:03 -0400

    React setup

---
+++ Section_3_Base_Features_Syntax.js
@@ -0,0 +1,21 @@
+/*      Setting up a local React project:
+
+.Use a dependency management tool (npm / yarn)
+.Use a bundler for optimal file organization (webpack)
+.Use a compiler that supports next-gen JS (babel + presets)
+.Use a development server to test the front end locally 
+
+Using Create-React App - 
+
+Global Install -
+1. npm requires Node.js, can install directly from website
+2. npm install -g create-react-app
+
+New Project -
+1. create-react-app <your-app-here> ( --scripts-version 1.1.5 ) 
+2. cd <your-app-here>/
+3. npm start 
+
+
+
+*/
