bfeb24f60f04176f0a64ac57c09f7f46a2e4f125
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-09-22 17:26:47 -0400

    React Dynamic Content

--- react-playground/src/Person/Person.js
+++ react-playground/src/Person/Person.js
@@ -3,6 +3,6 @@ import React from 'react';
 // Functional React Component //
 
 const person = () => {
-    return <p>I'm testing this.</p>
-}
+    return <p>I'm a person and I am {Math.floor(Math.random() * 30)}</p>
+};
 export default person;
