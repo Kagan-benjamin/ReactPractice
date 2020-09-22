4e109566c33abc224eb67556befb2103e6ebcc7d
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-09-22 18:20:50 -0400

    React Event Handling

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Ben', age: 30 },
+      { name: 'Layla', age: 32 }
+    ]
+  }
+
+  switchNameHandler = e => {
+    console.log('Button was clicked');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person name='Ben' age='30'> Extra Text </Person>
-        <Person name='Layla' age='32'/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> Extra Text as children </Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, 
