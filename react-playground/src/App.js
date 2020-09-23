699a7feee7ec77fa164d1a68e61891d08c872542
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-09-23 14:46:28 -0400

    React Styling

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -36,15 +36,25 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'baby blue',
+      font: 'inherit',
+      border: '1px solid white',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button
+          style={style} 
+          onClick={this.switchNameHandler}>Switch Name</button>
         <Person 
         name={this.state.persons[0].name} 
         age={this.state.persons[0].age}
         click={this.switchNameHandler}
-        changed={this.nameChangedHandler}> Extra Text as children </Person>
+        changed={this.nameChangedHandler}/>
         <Person 
         name={this.state.persons[1].name} 
         age={this.state.persons[1].age}
